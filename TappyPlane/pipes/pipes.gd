5ea245468be3ddf1aa4e12c4f0e0a8600cf2a5ad
Kevin S <kevinschanz@live.com>
2024-01-07 15:53:44 -0500

    [Tappy] Spawn infinite pipes at random Y-axis coordinates (#5)
    - Fixes the width of the `VisibleOnScreenEnabler2D` for the pipes so that the 
      pipes will scroll correctly (otherwise they just spawn off-screen and immediately 
      become dead objects).

--- TappyPlane/pipes/pipes.gd
+++ TappyPlane/pipes/pipes.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
 
-const SCROLL_SPEED: float = 50.0
+const SCROLL_SPEED: float = 120.0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print("Pipe created ... %s (%s, %s)" % [name, position.x, position.y])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
