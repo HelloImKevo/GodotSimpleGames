ac8b33045361b005872a6e80133c1e58c60708ec
Kevin Schanz <kevinschanz@live.com>
2024-01-07 14:31:41 -0500

    [Tappy] Stop physics processing when Plane hits the ground

--- TappyPlane/plane_cb/plane_cb.gd
+++ TappyPlane/plane_cb/plane_cb.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 
 @onready var animation_player = $AnimationPlayer
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 
 const GRAVITY: float = 1900.0
@@ -20,9 +21,18 @@ func _physics_process(delta):
 	fly()
 	
 	move_and_slide()
+	
+	if is_on_floor() == true:
+		die()
 
 
 func fly() -> void:
 	if Input.is_action_just_pressed("fly") == true:
 		velocity.y = POWER
 		animation_player.play("fly")
+
+
+func die() -> void:
+	animated_sprite_2d.stop()
+	# Stop physics engine for the Plane.
+	set_physics_process(false)
