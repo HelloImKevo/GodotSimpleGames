d0999247cf7db7b09b2bba37dd649776ffedf5a0
Kevin Schanz <kevinschanz@live.com>
2024-01-08 16:47:10 -0500

    [Tappy] Show a special message every 10 points scored

--- TappyPlane/hud/hud.gd
+++ TappyPlane/hud/hud.gd
@@ -2,6 +2,8 @@ extends Control
 
 
 @onready var score_label = $MC/ScoreLabel
+@onready var message_label = $MC2/MessageLabel
+@onready var timer = $Timer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,4 +12,23 @@ func _ready():
 
 
 func on_score_updated() -> void:
-	score_label.text = str(GameManager.get_score())
+	var score: int = GameManager.get_score()
+	score_label.text = str(score)
+	
+	if (score % 30 == 0) == true:
+		_show_message("Awesome!")
+	elif (score % 20 == 0) == true:
+		_show_message("Well done!")
+	elif (score % 10 == 0) == true:
+		_show_message("Good job!")
+
+
+func _show_message(msg: String) -> void:
+	message_label.text = msg
+	message_label.show()
+	timer.start()
+
+
+func _on_timer_timeout():
+	# Hide the message after it is shown briefly.
+	message_label.hide()
