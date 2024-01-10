879e25859f6a743b6bb6bb1c8cb5f47d0a34cd69
Kevin Schanz <kschanz@imobile3.com>
2024-01-09 19:00:35 -0500

    [Angry] Regenerate the Animal when it falls off the visible screen

--- AngryAnimals/animal/animal.gd
+++ AngryAnimals/animal/animal.gd
@@ -1,6 +1,13 @@
 extends RigidBody2D
 
 
+var _dead: bool = false
+
+
+func _to_string():
+	return "%s@%s" % ["Animal", get_rid()]
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,7 +21,21 @@ func _physics_process(delta):
 func update_debug_label() -> void:
 	var s = "g_pos:%s" % [Utils.vec2_to_str(global_position)]
 	s += "\nang:%.2f linear:%s" % [
-		angular_velocity, 
+		angular_velocity,
 		Utils.vec2_to_str(linear_velocity)
-	] 
+	]
 	SignalManager.on_update_debug_label.emit(s)
+
+
+func _on_screen_exited():
+	print("%s -> _on_screen_exited() -> die()" % [_to_string()])
+	die()
+
+
+func die() -> void:
+	if _dead == true:
+		return
+	
+	_dead = true
+	SignalManager.on_animal_died.emit()
+	queue_free()
--- AngryAnimals/singletons/signal_manager.gd
+++ AngryAnimals/singletons/signal_manager.gd
@@ -3,3 +3,4 @@ extends Node
 
 # TODO: Implement a data class container
 signal on_update_debug_label(text: String)
+signal on_animal_died
