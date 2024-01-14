860158c4b683b7202ae7dc1c20b3c9b01443eaa0
Kevin S <kevinschanz@live.com>
2024-01-14 13:32:09 -0500

    [Memory] Programmatically create Level Buttons and build a Sound Manager with basic volume control

---
+++ MemoryMadness/singletons/game_manager.gd
@@ -0,0 +1,14 @@
+extends Node
+## GameManager : game_manager.gd
+##
+## Singleton that manages game environment attributes.
+
+
+const LEVELS: Dictionary = {
+	1: { "rows": 2, "cols": 2 },
+	2: { "rows": 3, "cols": 4 },
+	3: { "rows": 4, "cols": 4 },
+	4: { "rows": 4, "cols": 6 },
+	5: { "rows": 5, "cols": 6 },
+	6: { "rows": 6, "cols": 6 }
+}
---
+++ MemoryMadness/singletons/sound_manager.gd
@@ -0,0 +1,42 @@
+extends Node
+## SoundManager : sound_manager.gd
+##
+## Singleton that manages playing sounds. This is where sound volumes (dB)
+## could be managed.
+
+
+const SOUND_MAIN_MENU = "main"
+const SOUND_IN_GAME = "ingame"
+const SOUND_SUCCESS = "success"
+const SOUND_GAME_OVER = "gameover"
+const SOUND_SELECT_TILE = "tile"
+const SOUND_SELECT_BUTTON = "button"
+
+const PREFIX = "res://assets/sounds/"
+
+const SOUNDS = {
+	SOUND_MAIN_MENU: preload(PREFIX + "bgm_action_3.mp3"),
+	SOUND_IN_GAME: preload(PREFIX + "bgm_action_4.mp3"),
+	SOUND_SUCCESS: preload(PREFIX + "sfx_sounds_fanfare3.wav"),
+	SOUND_GAME_OVER: preload(PREFIX + "sfx_sounds_powerup12.wav"),
+	SOUND_SELECT_TILE: preload(PREFIX + "sfx_sounds_impact1.wav"),
+	SOUND_SELECT_BUTTON: preload(PREFIX + "sfx_sounds_impact7.wav")
+}
+
+
+# Default volume (in decibels) for UI effects.
+var _ui_volume: float = -20.0
+
+
+func play_sound(player: AudioStreamPlayer, key: String) -> void:
+	if !SOUNDS.has(key):
+		return
+	
+	player.stop()
+	player.stream = SOUNDS[key]
+	player.play()
+
+
+func play_button_click(player: AudioStreamPlayer) -> void:
+	player.set_volume_db(_ui_volume)
+	play_sound(player, SOUND_SELECT_BUTTON)
