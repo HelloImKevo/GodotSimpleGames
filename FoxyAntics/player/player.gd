89d465313c2ad3accc6e2f8433d901ad83d55358
Kevin Schanz <kschanz@imobile3.com>
2024-01-18 18:47:52 -0500

    [Foxy] Set up initial player Input Map and add movement and jumping logic

--- FoxyAntics/player/player.gd
+++ FoxyAntics/player/player.gd
@@ -5,14 +5,28 @@ extends CharacterBody2D
 ## The player-controlled Fox character. Can run and jump.
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const RUN_SPEED = 300.0
+# Velocity applied in the upward direction (gravitational velocity
+# is applied as a downward constant).
+const JUMP_VELOCITY = -300.0
+const TERMINAL_VELOCITY = 400.0
+# Defines an invincibility window (iFrames).
+const HURT_TIME: float = 0.3
 
 @onready var sprite_2d = $Sprite2D
 @onready var animation_player = $AnimationPlayer
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
+# Default value of 980 (9.8 m/s^2).
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _to_string():
+	return "%s@%s" % ["Player_Fox", get_rid()]
+
+
+func _ready():
+	print(_to_string(), " Gravity: %s" % [gravity])
 
 
 func _physics_process(delta):
@@ -20,6 +34,29 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+	_handle_movement_input()
+
+	move_and_slide()
+
+
+func _handle_movement_input() -> void:
+	velocity.x = 0
+	
+	# Handle directional movement.
+	if Input.is_action_pressed("left"):
+		velocity.x = -RUN_SPEED
+	elif Input.is_action_pressed("right"):
+		velocity.x = RUN_SPEED
+	
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	velocity.y = clampf(velocity.y, JUMP_VELOCITY, TERMINAL_VELOCITY)
+
+
+# Unused. For reference purposes.
+func _default_godot_2d_input() -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -28,8 +65,6 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * RUN_SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		velocity.x = move_toward(velocity.x, 0, RUN_SPEED)
