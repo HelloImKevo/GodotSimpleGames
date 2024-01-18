64d7cef376e8f47d8b8cb183826539d13d059eaf
Kevin Schanz <kschanz@imobile3.com>
2024-01-18 18:09:23 -0500

    [Foxy] Create initial player CharacterBody2D and define idle, run, jump and fall animations

---
+++ FoxyAntics/level_base/level_base.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+@onready var player_cam = $PlayerCam
+@onready var player = $Player
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _physics_process(_delta):
+	# Anchor the 2D camera viewport to the player's position.
+	# We want all the physics processing to be calculated before
+	# the camera starts moving around.
+	player_cam.position = player.position
