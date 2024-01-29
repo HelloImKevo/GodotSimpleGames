80084574f76492b9f2356227c9de108926d99f1d
Kevin S <kevinschanz@live.com>
2024-01-29 16:08:10 -0500

    [Sokoban] Create a LevelButton extends NinePatchRect

---
+++ Sokoban/level_button/level_button.gd
@@ -0,0 +1,26 @@
+class_name LevelButton
+extends NinePatchRect
+## LevelButton : Simple square button that displays the Level Number and a Check Mark
+## if the level has been completed by the player.
+
+
+@onready var level_label = $LevelLabel
+@onready var check_mark = $CheckMark
+
+const GREEN_TEXTURE = preload("res://assets/green_panel.png")
+
+var _level_number: String = "99"
+
+
+func _ready():
+	level_label.text = _level_number
+
+
+func set_level_number(level_number: String) -> void:
+	_level_number = level_number
+
+
+func _on_gui_input(event: InputEvent):
+	if event.is_action_pressed("select"):
+		texture = GREEN_TEXTURE
+		print("Selected")
