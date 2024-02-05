d86111e8ae67a9806282c3556d32d7f2f6208e33
Kevin Schanz <kevinschanz@live.com>
2024-02-05 11:14:58 -0500

    [Ace] Saucer can take damage and has a Health Bar that will not Rotate along Path2D

--- SpaceAce/enemies/saucer.gd
+++ SpaceAce/enemies/saucer.gd
@@ -6,9 +6,13 @@ extends PathFollow2D
 const SPEED: float = 0.08
 const SHOOT_PROGRESS: float = 0.02
 const FIRE_OFFSETS = [0.25, 0.5, 0.75]
+const BOOM_DELAY: float = 0.2
+const HIT_DAMAGE: int = 40
 
 @onready var sound = $Sound
 @onready var state_machine = $AnimationTree["parameters/playback"]
+@onready var health_bar = $HealthBar
+@onready var booms = $Booms
 
 var missile_scene: PackedScene = preload("res://homing_missile/homing_missile.tscn")
 
@@ -51,3 +55,24 @@ func shoot() -> void:
 	var missile: HomingMissile = missile_scene.instantiate()
 	get_tree().root.add_child(missile)
 	missile.global_position = global_position
+
+
+func die() -> void:
+	queue_free()
+
+
+func make_booms() -> void:
+	for b in booms.get_children():
+		ObjectMaker.create_boom(b.global_position)
+		await get_tree().create_timer(BOOM_DELAY).timeout
+
+
+func _on_health_bar_died():
+	# We only want to observe this event once. After the enemy has died,
+	# subsequent signals will be ignored.
+	health_bar.disconnect("died", _on_health_bar_died)
+	state_machine.travel("death")
+
+
+func _on_area_2d_area_entered(area):
+	health_bar.take_damage(HIT_DAMAGE)
