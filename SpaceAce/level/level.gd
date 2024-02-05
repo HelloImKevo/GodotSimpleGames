da2788c4468b39281b8330b9cbc826cf858e9b17
Kevin Schanz <kevinschanz@live.com>
2024-02-05 13:11:34 -0500

    [Ace] Add background Music to Title and Level scenes

--- SpaceAce/level/level.gd
+++ SpaceAce/level/level.gd
@@ -1,9 +1,13 @@
 extends Node2D
 ## Level : A common Space Ace level.
 
+@onready var music = $Music
+
 
 func _ready():
+	music.volume_db = SoundManager.get_sfx_volume()
 	ScoreManager.reset_score()
+	SignalManager.on_player_died.connect(on_player_died)
 
 
 func _process(_delta):
@@ -11,3 +15,7 @@ func _process(_delta):
 		GameManager.load_main_scene()
 	if Input.is_action_just_pressed("test"):
 		ObjectMaker.create_powerup(Vector2(200.0, 200.0), GameData.POWERUP_TYPE.SHIELD)
+
+
+func on_player_died() -> void:
+	music.stop()
--- SpaceAce/main/main.gd
+++ SpaceAce/main/main.gd
@@ -2,9 +2,16 @@ extends Node2D
 ## Main : The Main Menu (Title) scene with PLAY and EXIT buttons.
 
 
+@onready var music = $Music
+
+
 func _ready():
-	pass # Replace with function body.
+	music.volume_db = SoundManager.get_sfx_volume()
 
 
 func _on_play_button_pressed():
 	GameManager.load_level_scene()
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
