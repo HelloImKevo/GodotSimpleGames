d70da44519ae73056388d8e68a8da32ce7df343c
Kevin Schanz <kevinschanz@live.com>
2024-02-05 16:39:23 -0500

    [Sneaky] NPC will now walk towards the mouse click using NavigationAgent2D and get stuck on wall corners

--- SneakyAgent/npc/npc.gd
+++ SneakyAgent/npc/npc.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 ## NPC : Non-Playable Character that uses Godot's NavigationAgent2D for pathfinding.
 
 
+const SPEED: float = 80.0
+
 @onready var nav_agent = $NavAgent
 @onready var sprite_2d = $Sprite2D
 @onready var debug_label = $DebugLabel
@@ -16,7 +18,19 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("set_target"):
 		nav_agent.target_position = get_global_mouse_position()
 	
+	update_navigation()
 	set_label()
+
+
+func update_navigation() -> void:
+	if nav_agent.is_navigation_finished():
+		return
+	
+	var next_path_position: Vector2 = nav_agent.get_next_path_position()
+	sprite_2d.look_at(next_path_position)
+	# Normalize vector of magnitude 1.
+	velocity = global_position.direction_to(next_path_position) * SPEED
+	move_and_slide()
 
 
 func set_label():
