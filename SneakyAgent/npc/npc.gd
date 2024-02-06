914def88f73bec6eaff96f39abe3c4abacb3db08
Kevin Schanz <Kevin.Schanz@globalpay.com>
2024-02-06 18:39:12 -0500

    [Sneaky] Make the NPC patrol between 4 Marker2D Waypoints

--- SneakyAgent/npc/npc.gd
+++ SneakyAgent/npc/npc.gd
@@ -3,22 +3,50 @@ extends CharacterBody2D
 ## NPC : Non-Playable Character that uses Godot's NavigationAgent2D for pathfinding.
 
 
+@export var patrol_points: NodePath
+
 const SPEED: float = 250.0
 
 @onready var nav_agent = $NavAgent
 @onready var sprite_2d = $Sprite2D
 @onready var debug_label = $DebugLabel
 
+var _waypoints: Array = []
+var _current_wp: int = 0
+
+# Reference error that can be resolved by using call_deferred("set_physics_process", true)
+'''
+E 0:00:01:0603   npc.gd:66 @ set_label(): NavigationServer map query failed because it was made before first map synchronization.
+  <C++ Error>    Condition "map_update_id == 0" is true. Returning: Vector<Vector3>()
+  <C++ Source>   modules/navigation/nav_map.cpp:119 @ get_path()
+  <Stack Trace>  npc.gd:66 @ set_label()
+				 npc.gd:39 @ _physics_process()
+'''
+
 
 func _ready():
-	pass # Replace with function body.
+	set_physics_process(false)
+	create_wp()
+	call_deferred("set_physics_process", true)
+	# call_deferred("set_temp_target")
 
 
-func _physics_process(delta):
+# Unused: For testing purposes.
+func set_temp_target():
+	nav_agent.target_position = Vector2(2800.0, 300.0)
+
+
+func create_wp() -> void:
+	for c: Node2D in get_node(patrol_points).get_children():
+		_waypoints.append(c.global_position)
+
+
+func _physics_process(_delta):
 	if Input.is_action_just_pressed("set_target"):
 		nav_agent.target_position = get_global_mouse_position()
 	
 	update_navigation()
+	process_patrolling()
 	set_label()
 
 
@@ -31,6 +59,18 @@ func update_navigation() -> void:
 	# Normalize vector of magnitude 1.
 	velocity = global_position.direction_to(next_path_position) * SPEED
 	move_and_slide()
+
+
+func process_patrolling() -> void:
+	if nav_agent.is_navigation_finished():
+		navigate_wp()
+
+
+func navigate_wp() -> void:
+	if _current_wp >= len(_waypoints):
+		_current_wp = 0
+	nav_agent.target_position = _waypoints[_current_wp]
+	_current_wp += 1
 
 
 func set_label():
